2823f853353304bf65f0f82e036009524e5d91c3
gavingarza <gaving@live.com>
2020-04-30 16:06:49 +0900

    added routes and resources to library controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  # Root
   root to: 'library#index'
+  resources :library
+  # # Read all
+  # get '/books', to: 'library#index'
 
-  get 'add', to: 'library#add'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # Read one
+  # get '/books/:id', to: 'library#show'
+
+  # # Create new book
+  # get '/books/add', to: 'library#new'
+  # post '/books', to: 'library#create'
+
+  # # Update
+  # get '/books/:id/edit', to: 'library#edit'
+  # patch '/books/:id', to: 'library#update'
+
+  # # Delete
+  # delete '/books/:id', to: 'library#destroy'
+
 end
