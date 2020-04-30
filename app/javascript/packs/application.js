250d3a15622bcb57aa85eae170b1ff8e8593b958
gavingarza <gaving@live.com>
2020-04-30 11:58:54 +0900

    adding scroll functionality with javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,11 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
+
 import "bootstrap";
